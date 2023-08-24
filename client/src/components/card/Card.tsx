90d0e37789c10fa9587cd69c611251492b26089c
brand421 <r3v9191@gmail.com>
2023-08-24 18:03:59 -0400

    add server side, add card component

---
+++ client/src/components/card/Card.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+function Card() {
+  const [data, setData] = useState({
+    name: String,
+    review: Array,
+    description: String,
+  });
+  return (
+    <div>
+      <h2></h2>
+    </div>
+  );
+}
+
+export default Card;
