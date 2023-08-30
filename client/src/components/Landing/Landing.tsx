a8dc0d98527ee5b679ba579adce6c82200520b3d
brand421 <57531511+brand421@users.noreply.github.com>
2023-08-30 10:51:50 -0400

    update landing page, include fetch request for mapbox api

--- client/src/components/Landing/Landing.tsx
+++ client/src/components/Landing/Landing.tsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./landing.css";
 import { Button } from "@mui/base";
 import logo from "./TAlogo.png";
-import { SearchBoxCore, SessionToken } from "@mapbox/search-js-core";
+import { SessionToken } from "@mapbox/search-js-core";
 
-async function Landing() {
+function Landing() {
   const [input, setInput] = useState("");
   const [cityList, setCityList] = useState([
     {
@@ -13,32 +13,25 @@ async function Landing() {
     },
   ]);
 
-  const search = new SearchBoxCore({
-    accessToken: process.env.REACT_APP_MAPBOX_API,
-  });
   const sessionToken = new SessionToken();
 
-  // const [card, setCard] = useState<Results | undefined>(undefined);
-
-  // const fetchData = async (input: string) => {
-  //   await fetch(`http://localhost:5000/${input}`).then(async (res) => {
-  //     const response: Results = await res.json();
-  //     setCard(response);
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   fetchData(search);
-  // }, [search]);
+  useEffect(() => {
+    fetch(
+      `https://api.mapbox.com/search/searchbox/v1/suggest?q=${input}&language=en&types=locality,city&session_token=${sessionToken}&access_token=${process.env.REACT_APP_MAPBOX_API}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCityList(data);
+      });
+      .catch((err) => {
+        console.log(err.message)
+      });
+  }), [];
 
   const handleChange = (e: any) => {
     setInput(e.target.value);
-
-    console.log(input);
   };
-
-  const results = await search.suggest(input, { sessionToken });
-  if (results.suggestions.length === 0) return;
 
   // const cityList: any = (search: string) => {
   //   let results = `https://api.mapbox.com/search/searchbox/v1/suggest?q=${search}&access_token=${process.env.REACT_APP_MAPBOX_API}&session_token=0c1ad424-ebbc-4b8d-9b1b-f3312c1fbf10&language=en&limit=5&types=locality%2Cplace`;
@@ -63,11 +56,9 @@ async function Landing() {
       </form>
       <div className="results__container">
         <div className="results__list">
-          <div className="result__header">
-            {results.suggestions.map((x) => x.name)}
-          </div>
+          <div className="result__header">{cityList.map((x) => x.name)}</div>
           <div className="result__subheader">
-            {results.suggestions.map((x) => x.place_formatted)}
+            {cityList.map((x) => x.place_formatted)}
           </div>
         </div>
       </div>
