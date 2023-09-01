dd1d6ed19c445f3bf9989470410ffafc2dd67e0f
brand421 <57531511+brand421@users.noreply.github.com>
2023-09-01 14:43:20 -0400

    create submit city to json function on landing page

--- client/src/components/Landing/Landing.tsx
+++ client/src/components/Landing/Landing.tsx
@@ -2,30 +2,33 @@ import React, { useEffect, useState } from "react";
 import "./landing.css";
 import { Button } from "@mui/base";
 import logo from "./TAlogo.png";
+import axios from "axios";
 import { SessionToken } from "@mapbox/search-js-core";
 import { SearchBox } from "@mapbox/search-js-react";
 
 function Landing() {
-  // const [search, setSearch] = useState("");
-  const [cityList, setCityList] = useState<[]>([]);
+  const [search, setSearch] = useState({
+    city: "",
+  });
+  // const [cityList, setCityList] = useState<[]>([]);
 
-  const handleChange = (e: any) => {
-    const search = e.target.value;
-    const sessionToken = new SessionToken();
-    fetch(
-      `https://api.mapbox.com/search/searchbox/v1/suggest?q=${search}&language=en&types=locality,city&session_token=${sessionToken}&access_token=${mapboxToken}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setCityList(data);
-        console.log(cityList);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
+  // const handleChange = (e: any) => {
+  //   const search = e.target.value;
+  //   const sessionToken = new SessionToken();
+  //   fetch(
+  //     `https://api.mapbox.com/search/searchbox/v1/suggest?q=${search}&language=en&types=locality,city&session_token=${sessionToken}&access_token=${mapboxToken}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setCityList(data);
+  //       console.log(cityList);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err.message);
+  //     });
+  // };
 
-  const mapboxToken = process.env.REACT_APP_MAPBOX_API;
+  // const mapboxToken = process.env.REACT_APP_MAPBOX_API;
   // useEffect(() => {
   // const sessionToken = new SessionToken();
   //   fetch(
@@ -41,14 +44,33 @@ function Landing() {
   //     });
   // }, [search]);
 
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    console.log(search);
+    axios
+      .post("http://localhost:5000/", { city: search })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
+
+  const handleChange = (e: any) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="landing__container">
       <h1 className="header">Where do you want to go?</h1>
       <h4>
         Powered by <img className="logo" src={logo} alt="tripadvisor logo" />
       </h4>
-      <form className="landing__form" action="/" method="POST">
-        <input id="city" onChange={handleChange} />
+      <form className="landing__form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="city"
+          name="city"
+          value={search.city}
+          onChange={handleChange}
+        />
         <Button type="submit" className="landing__submit">
           Submit
         </Button>
--- server/index.ts
+++ server/index.ts
@@ -6,13 +6,17 @@ const app = express()
 const bodyParser = require('body-parser')
 const axios = require('axios')
 const port = 5000
+const cors = require("cors")
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.resolve(__dirname, "../client/build")))
+app.use(express.json())
+app.use(cors())
 
 app.post("/", (req, res) => {
     let city = req.body;
     console.log(city);
+    res.send(city)
 })
 
 app.get("/", (req, res) => {
