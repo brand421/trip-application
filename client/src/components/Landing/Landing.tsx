9f3a56d9ba50e561f951ac915be32c4afd0bca27
brand421 <57531511+brand421@users.noreply.github.com>
2023-09-15 16:22:23 -0400

    add search results to landing page

--- client/src/components/Landing/Landing.tsx
+++ client/src/components/Landing/Landing.tsx
@@ -3,15 +3,23 @@ import { useNavigate } from "react-router";
 import "./landing.css";
 import { Button } from "@mui/base";
 import logo from "./TAlogo.png";
-import axios from "axios";
 import { motion } from "framer-motion";
 import { SessionToken } from "@mapbox/search-js-core";
 
 function Landing() {
+  interface cityResults {
+    name: string;
+    place: string;
+  }
+
   const navigate = useNavigate();
   const [search, setSearch] = useState<{ city: string }>({ city: "" });
+  const [results, setResults] = useState<cityResults[]>([
+    { name: "", place: "" },
+  ]);
+  const [submit, setSubmit] = useState<boolean>(false);
 
-  const container = {
+  const containerSearch = {
     hidden: { opacity: 0, x: "-100px" },
     show: {
       opacity: 1,
@@ -22,7 +30,18 @@ function Landing() {
     },
   };
 
-  const item = {
+  const containerResults = {
+    hidden: { opacity: 0, y: "50px" },
+    show: {
+      opacity: 1,
+      y: "0px",
+      transition: {
+        staggerChildren: 0.25,
+      },
+    },
+  };
+
+  const itemSearch = {
     hidden: { opacity: 0, x: "50px" },
     show: {
       opacity: 1,
@@ -30,13 +49,39 @@ function Landing() {
     },
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    axios.post("http://localhost:5000/", search).then((res) => {
-      console.log(res);
-      navigate("/searchresults");
-    });
+  const itemResults = {
+    hidden: { opacity: 0, y: "50px" },
+    show: {
+      opacity: 1,
+      y: "0px",
+    },
   };
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const mapboxToken = process.env.REACT_APP_MAPBOX_API;
+    const sessionToken = new SessionToken();
+    const newSearch = search.city.trim().replace(/ /g, "+");
+    const newResults: cityResults[] = [];
+    console.log(newSearch);
+    fetch(
+      `https://api.mapbox.com/search/searchbox/v1/suggest?q=${newSearch}&language=en&limit=5&types=city,locality&session_token=${sessionToken}&access_token=${mapboxToken}`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        for (let i = 0; i < data.suggestions.length; i++) {
+          newResults.push({
+            name: data.suggestions[i].name,
+            place: data.suggestions[i].place_formatted,
+          });
+        }
+        console.log(newResults);
+        setResults(newResults);
+        setSubmit(true);
+      });
+  }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let search = e.currentTarget.value;
@@ -44,36 +89,81 @@ function Landing() {
   };
 
   return (
-    <motion.div
-      variants={container}
-      initial="hidden"
-      animate="show"
-      exit={{ opacity: 0, x: "100px" }}
-      className="landing__container"
-    >
-      <motion.div variants={item} initial="hidden" animate="show" exit="hidden">
-        <h1 className="header">Where Are You Headed?</h1>
-        <h4>
-          Powered by <img className="logo" src={logo} alt="tripadvisor logo" />
-        </h4>
+    <>
+      <motion.div
+        variants={containerSearch}
+        initial="hidden"
+        animate="show"
+        exit={{ opacity: 0, x: "100px" }}
+        className="landing__container"
+      >
+        <motion.div
+          variants={itemSearch}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          <h1 className="header">Where Are You Headed?</h1>
+          <h4>
+            Powered by{" "}
+            <img className="logo" src={logo} alt="tripadvisor logo" />
+          </h4>
+        </motion.div>
+        <motion.div
+          variants={itemSearch}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+        >
+          <form className="landing__form" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              id="city"
+              name="city"
+              value={search.city}
+              onChange={handleChange}
+              className="landing__input"
+              required
+            />
+            <Button type="submit" className="landing__submit">
+              Submit
+            </Button>
+          </form>
+        </motion.div>
       </motion.div>
-      <motion.div variants={item} initial="hidden" animate="show" exit="hidden">
-        <form className="landing__form" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            value={search.city}
-            onChange={handleChange}
-            className="landing__input"
-            required
-          />
-          <Button type="submit" className="landing__submit">
-            Submit
-          </Button>
-        </form>
-      </motion.div>
-    </motion.div>
+      <div>
+        {submit ? (
+          <div className="search__container">
+            <div>
+              <h1 className="header">Which City Did You Want?</h1>
+            </div>
+            <div className="city__block">
+              <motion.div
+                variants={containerResults}
+                initial="hidden"
+                animate="show"
+                className="cities__list"
+              >
+                {results.map((x, key) => (
+                  <motion.div
+                    variants={itemResults}
+                    key={key}
+                    className="city__list"
+                  >
+                    <a href="#">
+                      <h3 className="city__name">{x.name}</h3>
+                      <p className="country__name">{x.place}</p>
+                    </a>
+                  </motion.div>
+                ))}
+              </motion.div>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </>
   );
 }
 
